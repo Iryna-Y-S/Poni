f2554afcbcb17bf0391788eec0ee6043b0059f53
Iryna-Y-S <baluta_irina@mail.ru>
2014-07-09 12:06:45 +0300

    Pony1.1

--- pony-modul.js
+++ pony-modul.js
@@ -1,30 +1,36 @@
 var Pony = (function () {
+
     function Constructor () {
+	
 	    var attributes = {},
 		    key;
+			
 		this.setAttributes = function (key, value) {
 		    attributes[key] = value;
-			};
+	    };
+		
 		this.getAttributes = function () {
 		   return attributes;
-			};
+		};
+		
+		return this;
 	}
 
 	Constructor.prototype.ride = function () {
-	    console.log("Pony rides!")
+	    console.log("Pony rides!");
 	};
+	
 	Constructor.prototype.sayIgogo = function () {  
-	    console.log("Pony says:'Igogo!'")
+	    console.log("Pony says:'Igogo!'");
 	};
+	
 	Constructor.prototype.eat = function () {
 	    if (fillHungry()) {
 		    console.log("Pony is hungry!");
 		} else {
-		    console.log("Pony isn't hungry!")
+		    console.log("Pony isn't hungry!");
 		}	
 	};
-	
-	fillHungry();
 	
 	function fillHungry () {
 	    return true;
